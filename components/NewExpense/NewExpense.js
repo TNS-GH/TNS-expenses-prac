6bf68d3b975e5b168ff998e7e4d066d349291595
TNS-GH <130904104+TNS-GH@users.noreply.github.com>
2023-04-22 16:02:55 +0800

    Add files via upload

---
+++ components/NewExpense/NewExpense.js
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+
+import "./NewExpense.css";
+import ExpenseForm from "./ExpenseForm";
+
+function NewExpense(props) {
+  const [showForm, setShowForm] = useState(false);
+
+  function saveExpenseDataHandler(enteredExpenseData) {
+    const expenseData = {
+      ...enteredExpenseData,
+      id: Math.random().toString(),
+    };
+    props.onAddExpense(expenseData);
+    setShowForm((prevShowForm) => !prevShowForm);
+  }
+
+  function showFormClickHandler() {
+    setShowForm((prevShowForm) => !prevShowForm);
+  }
+
+  return (
+    <div className="new-expense">
+      {!showForm && (
+        <button onClick={showFormClickHandler}>Add New Expense</button>
+      )}
+      {showForm && (
+        <ExpenseForm
+          onSaveExpenseData={saveExpenseDataHandler}
+          onCancel={showFormClickHandler}
+        />
+      )}
+    </div>
+  );
+}
+
+export default NewExpense;
